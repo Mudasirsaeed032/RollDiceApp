3e28972ae76d61d360fbaf9ad8ed5e06dc1f35a0
MudasirSaeed032 <mudasir.saeed032@gmail.com>
2025-02-18 02:41:33 +0500

    adding custom widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,31 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    const MaterialApp(
-      home: Scaffold(
-        body: Center(
-            child: Text('This is ugly as mentioned'),
-            ),
-          ),
-    ),
-  );
+  runApp(MaterialApp(home: Scaffold(body: GradientContainer())));
+}
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            const Color.fromARGB(255, 79, 6, 6),
+            const Color.fromARGB(255, 179, 9, 9),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: Center(
+        child: Text(
+          'This is not mentioned',
+          style: TextStyle(color: Colors.white, fontSize: 28.5),
+        ),
+      ),
+    );
+  }
 }
